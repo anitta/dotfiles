ed8f120ee7915aef810b4649af1d9ed86cbe4200
Akinori Nitta <nitta.akinori@gmail.com>
2024-06-09 21:57:47 +0900

    support homebrew

--- setup.sh
+++ setup.sh
@@ -4,8 +4,14 @@ alacritty() {
   cp -r aralacritty/ ~/.config/alacritty
 }
 
+homebrew() {
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  brew bundle
+}
+
 setup(){
   alacritty
+  homebrew
 }
 
 setup
